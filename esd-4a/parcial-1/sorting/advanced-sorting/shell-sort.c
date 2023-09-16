e58ba8d8f836a921a800958008f197a183847074
Robot Asimov <th3r0b0t4s1m0v@gmail.com>
2023-09-16 12:43:49 -0600

    add merge and shell sort c file in advanced

---
+++ esd-4a/parcial-1/sorting/advanced-sorting/shell-sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void setArray (int *array, int arraySize)
+{
+    for (int i = 0; i < arraySize; i++)
+    {
+        array[i] = rand()%20+1;
+    }
+}
+
+void printArray (int *array, int arraySize)
+{
+    for (int i = 0; i < arraySize; i++)
+    {
+        printf("%5d", array[i]);
+    }
+    printf("\n");
+}
+
+void shellSort (int *array)
+{
+    
+}
+
+int main (void)
+{
+    int array[10];
+    int arraySize = 10;
+
+    srand(time(NULL));
+
+    setArray(array, arraySize);
+    printArray(array, arraySize);
+    // sort function
+
+    printArray(array, arraySize);
+
+    return 0;
+}
