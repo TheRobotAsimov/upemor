0386a4de1be5632e65477d0573733f66d5fb3652
Robot Asimov <th3r0b0t4s1m0v@gmail.com>
2023-11-23 16:17:23 -0600

    Binary search tree

---
+++ esd-4a/parcial-3/tree_automatic.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+} node;
+
+void create_tree (node **root);
+node *insert (int data);
+void insert2 (node **root, int data);
+void print_tree (node *tree, int space);
+
+// Traversals
+/* 
+    Preorder:
+    - Node
+    - Left
+    - Right
+
+    Inorder:
+    - Left
+    - Node
+    - Right
+
+    Postorder:
+    - Left
+    - Right
+    - Node
+
+ */
+
+void preorder (node *tree, int space);
+void inorder (node *tree, int space);
+
+int main (void)
+{
+    node *root;
+
+    create_tree (&root);
+
+    /* root = insert (5);
+    root -> left = insert (10);
+    root -> right = insert (20);
+    root -> left -> left = insert (30);
+    root -> left -> right = insert (40);
+    root -> right -> left = insert (50);
+    root -> right -> right = insert (60); */
+
+    int data, times;
+
+    printf("Ingrese la cantidad de nodos: ");
+    scanf("%d", &times);
+
+    for (int i = 0; i < times; i++)
+    {
+        printf("Ingrese el dato: ");
+        scanf("%d", &data);
+        insert2(&root, data);
+    }
+
+    printf("\n");
+
+    print_tree(root, 0);
+
+    printf("\n---------------------------------\n\n");
+
+    printf("PREORDEN:\n\n ");
+    preorder(root, 0);
+
+    printf("\n---------------------------------\n\n");
+
+    printf("INORDEN:\n\n ");
+    inorder(root, 0);
+}
+
+void create_tree (node **root)
+{
+    *root = NULL;
+}
+
+node *insert (int data)
+{
+    node *new_node = (node *) malloc (sizeof (node));
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+void print_tree (node *tree, int space)
+{
+    if (tree != NULL)
+    {
+        print_tree (tree -> right, space + 1);
+        for (int i = 0; i < space; i++)
+        {
+            printf("\t");
+        }
+        printf ("%d\n", tree -> data);
+        print_tree (tree -> left, space + 1);
+    }
+}
+
+void insert2 (node **root, int data)
+{
+    // aux is equal to current and aux2 is equal to previous
+    //node *aux, *aux2;
+    int number;
+    //int option;
+
+    if (*root == NULL)
+    {
+        *root = insert (data);
+    }
+    else
+    {
+        //aux = *root;
+        number = (*root) -> data;
+
+        if (data < number)
+        {
+            insert2 (&(*root) -> left, data);
+        }
+        else
+        {
+            insert2 (&(*root) -> right, data);
+        }
+    }
+
+    //printf("\n Nodo creado\n\n");
+}
+
+void preorder (node *tree, int space)
+{
+    // Very important the conditional
+    if (tree)
+    {
+        for (int i = 0; i < space; i++)
+        {
+            printf("\t");
+        }
+        printf("%d\n", tree -> data);
+        preorder (tree -> left, ++space);
+        preorder (tree -> right, space);
+    }
+}
+
+void inorder (node *tree, int space)
+{
+    if (tree)
+    {
+        inorder(tree -> left, space + 1);
+        for (int i = 0; i < space; i++)
+        {
+            printf("\t");
+        }
+        printf("%d\n", tree -> data);
+        inorder(tree -> right, space + 1);
+    }
+}
